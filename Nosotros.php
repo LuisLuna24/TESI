0644583158d8b557b7a1f2a2493524587ad69d44
Torres <barrientostorres9@gmail.com>
2023-06-08 13:13:28 -0600

    add cabesera

---
+++ Nosotros.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/nosotros.css?see=1.4">
+    <title>¿QUIENES SOMOS? - CCAI TESE – Centro de Cooperación Academia Industria</title>
+</head>
+<body>
+<header class="header">
+    <?php
+     require('global/cabesera.php');
+   ?>
+   <div class="contenedor__elements">
+   <div class="contenedor__img">
+   <img src="./assets/images/Tesi-img.jpg" class="header__img">
+   </div>
+   <div class="contenedor-letras">
+        <h1>¿QUIENES SOMOS?</h1>
+    </div>  
+
+    </div>
+    </header>
+
+    
+</body>
+</html>
