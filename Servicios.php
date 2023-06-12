adabf65339c7509502289d4b092d3912b302b35a
LuisLuna24 <eduarlun4@gmail.com>
2023-06-12 17:01:01 -0600

    Modificando Servicios

--- Servicios.php
+++ Servicios.php
@@ -20,63 +20,102 @@
             </div>
         </div>
     </header>
+
 <!--===============================================================================================
                                             Parte 1
 ==================================================================================================-->
+    <section class="Actividades">
+        <div class="Actividades_Titulo">
+            <h1 class="Actividades_Titulo">Nuestras Actividades</h1>
+        </div>
+        <div class="Actividades_Contenedor">
+            <div class="Acividades_Card">
+                <img src="img/img1.jpg" alt="">
+                <div class="Actividades_Card_Contenido">
+                    <h1>Industria 4.0</h1>
+                    <p>En la industria 4.0 se cubre las nesecidades de las diferentes area dela salud como sistemas de modelados con realidad aumentada previos a cirugías, seguridad publica  como mapeo inteligente de zonas delictivas, empresas en general con desarrollo de aplicaciones y tecnologías acordes a las necesidades de la empresa entre otros servicios.</p>
+                </div>
+            </div>
+
+            <div class="Acividades_Card">
+                <img src="img/img4.jpg" alt="">
+                <div class="Actividades_Card_Contenido">
+                    <h1>Industria 4.0</h1>
+                    <p>En la industria 4.0 se cubre las nesecidades de las diferentes area dela salud como sistemas de modelados con realidad aumentada previos a cirugías, seguridad publica  como mapeo inteligente de zonas delictivas, empresas en general con desarrollo de aplicaciones y tecnologías acordes a las necesidades de la empresa entre otros servicios.</p>
+                </div>
+            </div>
+
+            <div class="Acividades_Card">
+                <img src="img/img5.jpg" alt="">
+                <div class="Actividades_Card_Contenido">
+                    <h1>Industria 4.0</h1>
+                    <p>En la industria 4.0 se cubre las nesecidades de las diferentes area dela salud como sistemas de modelados con realidad aumentada previos a cirugías, seguridad publica  como mapeo inteligente de zonas delictivas, empresas en general con desarrollo de aplicaciones y tecnologías acordes a las necesidades de la empresa entre otros servicios.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--===============================================================================================
+                                            Parte 2
+==================================================================================================-->
     <section class="Servicios">
-        <h1 class="Servicios-Titulo">Nuestros Servicios</h1>
-        <p class="Servicio_Contenido">El centro CAI busca brindar soluciones tecnológicas y mejorar las condiciones de ejecución de procesos de las empresas y distintos sectores de la<br> zona, con el objetivo de investigar, diseñar e innovar procesos y productos. <br> Las actividades que se pretender llevar a cabo son las siguientes:</p>
+        <h1 class="Servicios-Titulo">Actividades a Realizar</h1>
+        <p class="Servicio_Contenido">El centro CAI busca brindar soluciones tecnológicas y mejorar las condiciones de ejecución de procesos de las empresas y distintos sectores de la zona, con el objetivo de investigar, diseñar e innovar procesos y productos. <br> Las actividades que se pretender llevar a cabo son las siguientes:</p>
         <div class="Cards">
             <nav class="Servicios_Contenedor">
                 <img class="Imagen_Servicios" src="img/img1.jpg" alt="">
                 <div class="Servicios_Card">
-                    <div>
+                    <div class="Contenido">
                         <h1 class="Card_Titulo">Industria 4.0</h1>
                         <p class="Card_Contenido Contenido1">•	Desarrollo de software para los sectores gubernamentales, privados y públicos.<br>•	Desarrollo de plataformas web.<br>•	Automatización de procesos.<br>•	Servicios de mantenimiento de red y equipo de cómputo a empresas.<br>•	Servicios de ciberseguridad. <br>•	Capacitaciones para la zona oriente. </p>
                     </div>
-                    <div>
+                    <div class="Contenido">
                         <h1>Algunas de las empresas que se trabaja:</h1>
-                        <p class="Card_Contenido Contenido2"> •	Smart Force.<br>•	Crystal Software House.<br>•	Ilunjo S.A.</p>
+                        <p class="Card_Contenido "> •	Smart Force.<br>•	Crystal Software House.<br>•	Ilunjo S.A.</p>
                     </div>
-                    <div>
+            
+                    <div class="Contenido">
                         <h1>Con estas empresas se pretende trabajar:</h1>
                         <p class="Card_Contenido Contenido3">•	Asesoría en Tecnologías de la Industria 4.0<br>•	Yakult.<br>•	Cruz Roja.<br>•	Centros Educativos (Básico, Medio Superior y Superior).<br>•	Centros comerciales.<br>•	Plazas de cobro (Autopistas).<br>•	Unidades habitacionales.<br>•	Asentamientos de colonias nuevas. (Ayuntamiento).<br>•	Alimentos Especializados Gómez S.A de C.V. <br>•	Seguridad Pública.</p>
                     </div>
                 </div>
             </nav>
 
-            <nav class="Servicios_Contenedor">
-                <img class="Imagen_Servicios" src="img/img1.jpg" alt="">
-                <div class="Servicios_Card">
-                    <div>
-                        <h1 class="Card_Titulo">Industria 4.0</h1>
-                        <p class="Card_Contenido Contenido1">•	Desarrollo de software para los sectores gubernamentales, privados y públicos.<br>•	Desarrollo de plataformas web.<br>•	Automatización de procesos.<br>•	Servicios de mantenimiento de red y equipo de cómputo a empresas.<br>•	Servicios de ciberseguridad. <br>•	Capacitaciones para la zona oriente. </p>
-                    </div>
-                    <div>
-                        <h1>Algunas de las empresas que se trabaja:</h1>
-                        <p class="Card_Contenido Contenido2"> •	Smart Force.<br>•	Crystal Software House.<br>•	Ilunjo S.A.</p>
-                    </div>
-                    <div>
-                        <h1>Con estas empresas se pretende trabajar:</h1>
-                        <p class="Card_Contenido Contenido3">•	Asesoría en Tecnologías de la Industria 4.0<br>•	Yakult.<br>•	Cruz Roja.<br>•	Centros Educativos (Básico, Medio Superior y Superior).<br>•	Centros comerciales.<br>•	Plazas de cobro (Autopistas).<br>•	Unidades habitacionales.<br>•	Asentamientos de colonias nuevas. (Ayuntamiento).<br>•	Alimentos Especializados Gómez S.A de C.V. <br>•	Seguridad Pública.</p>
-                    </div>
-                </div>
-            </nav>
+            <div class="linea"></div>
 
             <nav class="Servicios_Contenedor">
-                <img class="Imagen_Servicios" src="img/img1.jpg" alt="">
                 <div class="Servicios_Card">
-                    <div>
-                        <h1 class="Card_Titulo">Industria 4.0</h1>
-                        <p class="Card_Contenido Contenido1">•	Desarrollo de software para los sectores gubernamentales, privados y públicos.<br>•	Desarrollo de plataformas web.<br>•	Automatización de procesos.<br>•	Servicios de mantenimiento de red y equipo de cómputo a empresas.<br>•	Servicios de ciberseguridad. <br>•	Capacitaciones para la zona oriente. </p>
+                    <div class="Contenido">
+                        <h1 class="Card_Titulo">Arqitectura 4.0</h1>
+                        <p class="Card_Contenido Contenido1">Las actividades que se han considerado a desarrollar son diseño y desarrollo de proyectos, procesos constructivos y estudios de mecánica de suelo. <br>1.	Autocad Estudiantil (Diseño 2D y 3D). <br>2.	SketchUp  (Animación). <br>3.	Lumion (Recorrido virtual). <br>4.	Revit BIM (Diseño Building Information Modeling). <br>5.	Opus / Neodata (Cálculo de Precios Unitarios y Presupuestos) <br>6.	Cyped / SAP 2000 (Cálculo de estructuras de concreto y acero) </p>
+                    </div>
+                    <div class="Contenido">
+                        <h1>Procesos Constructivos:</h1>
+                        <p class="Card_Contenido Contenido2"> 1.	Área hermética de almacenamiento para cemento. <br>2.	Área de almacenamiento para agregados básicos tales como arena y grava. <br>3.	Área de almacenamiento para acero de refuerzo. <br>4.	Tanque elevado de 1100 litros de agua potable con instalación hidráulica. <br> 5.	Pileta para curado de cilindros y vigas de prueba. <br>6.	Equipo y herramienta para las diferentes actividades.</p>
+                    </div>
+                    <div class="Contenido">
+                        <h1>Estudios de mecánica de suelos:</h1>
+                        <p class="Card_Contenido Contenido3"> Identificar tipo de suelo, capacidad de carga de suelo, limite plástico y elástico de este. <br>1.	Meseta de concreto para colocación de horno. <br>2.	Instalación eléctrica especial a 220 volts. <br>3.	Meseta para tamiz.  <br>4.	Meseta para Basculas de precisión. <br>5.	Tarja de lavado.  <br>6.	3 mesas de trabajo. </p>
+                    </div>
+                </div>
+                <img class="Imagen_Servicios" src="img/img4.jpg" alt="">
+            </nav>
+
+            <div class="linea"></div>
+
+            <nav class="Servicios_Contenedor">
+                <img class="Imagen_Servicios" src="img/img6.jpg" alt="">
+                <div class="Servicios_Card">
+                    <div class="Contenido">
+                        <h1 class="Card_Titulo">Gestión clínica hospitalaria </h1>
+                        <p class="Card_Contenido Contenido1">Actividades que se pretenden realizar: <br>•	Trabajar con actividades de Gestión clínica y regulación sanitaria actualizada con las empresas, hospitales públicos y privados </br>•	Realizar protocolo de revisión de mantenimiento preventivo y correctivo para equipos y dispositivos médicos.</p>
+                    </div>
+                    <div class="Contenido">
+                        <h1 >Algunas de las empresas que se trabaja:</h1>
+                        <p class="Card_Contenido Contenido2">•	Capacitación del personal usuario de los equipos y dispositivos médicos, para minimizar fallas y tener un mejor funcionamiento. <br>•	Revisión, creación y actualización de manuales de procedimientos de acuerdo con la normatividad vigente. </p>
                     </div>
                     <div>
-                        <h1>Algunas de las empresas que se trabaja:</h1>
-                        <p class="Card_Contenido Contenido2"> •	Smart Force.<br>•	Crystal Software House.<br>•	Ilunjo S.A.</p>
-                    </div>
-                    <div>
-                        <h1>Con estas empresas se pretende trabajar:</h1>
-                        <p class="Card_Contenido Contenido3">•	Asesoría en Tecnologías de la Industria 4.0<br>•	Yakult.<br>•	Cruz Roja.<br>•	Centros Educativos (Básico, Medio Superior y Superior).<br>•	Centros comerciales.<br>•	Plazas de cobro (Autopistas).<br>•	Unidades habitacionales.<br>•	Asentamientos de colonias nuevas. (Ayuntamiento).<br>•	Alimentos Especializados Gómez S.A de C.V. <br>•	Seguridad Pública.</p>
+                        <h1 class="Contenedor_Titulo">Con estas empresas se pretende trabajar:</h1>
+                        <p class="Card_Contenido Contenido3">•	Seguir manteniendo la colaboración con las empresas:  <br>•	Servicios de ingeniería BEIKHEL S.A. de C.V. <br>•	Human Corporis S.A. de C.V. <br>•	Assesory and Logistic on aplicated radiation assrane S.A. de C.V. <br>•	Hospital de Alta Especialidad de Ixtapaluca (HRAEI). <br>•	ISSSTE Tláhuac Heriberto Castillo 216. <br>•	Hospital General La Raza. </p>
                     </div>
                 </div>
             </nav>
@@ -85,23 +124,7 @@
         
     </section>
 
-<!--===============================================================================================
-                                            Parte 2
-==================================================================================================-->
-<section id="Explorar" class="Actividades">
-        <h1 class="Actividades_Titulo">Nuestras Actividades</h1>
-        <nav class="Actividades_Todas_Targetas">
-            <div class="Actividades_Targetas">
-                <div class="">
 
-                </div>
-                <div>
-
-                </div>
-            </div>
-        </nav>
-        
-    </section>
 <!--===============================================================================================
                                             Parte final
 ==================================================================================================-->
