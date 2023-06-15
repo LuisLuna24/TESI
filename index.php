bcbd18493aa345db66b49228feabd9e8c7659a38
LuisLuna24 <eduarlun4@gmail.com>
2023-06-14 18:40:15 -0600

    Modifique el comunicate del index

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
             <div class="Contacto_Texto">
                 <h1 class="Contacto_Titulo">Contactanos</h1>
                 <p class="Contacto_Texto">Sí, te interesa saber más sobre nuestros servicios</p>
-                <a href="contacto.php" class="Contacto_Boton">Contactanos</a>
+                <a href="contacto.php" class="cta"><span>Leer Mas</span><svg viewBox="0 0 13 10" height="10px" width="15px"><path d="M1,5 L11,5"></path><polyline points="8 1 12 5 8 9"></polyline></svg></a>
             </div>
         </nav>    
     </Section>
