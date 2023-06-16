1979252a0f58c365be9e7256a97ceb9ed9f9361b
LuisLuna24 <eduarlun4@gmail.com>
2023-06-15 19:01:01 -0600

    modifique registra para expandirlo

--- JS/SlayerCambiante.js
+++ JS/SlayerCambiante.js
@@ -1,45 +1,34 @@
-/**
-  Swiper:
-  https://swiperjs.com/
-**/
-const swiper = new Swiper(".swiperCarousel", {
-    slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween: 10,
-    keyboard: {
-      enabled: true,
-    },
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
-  const slides = document.getElementsByClassName("swiper-slide");
-  for (const slide of slides) {
-    slide.addEventListener("click", () => {
-      const { className } = slide;
-      if (className.includes("swiper-slide-next")) {
-        swiper.slideNext();
-      } else if (className.includes("swiper-slide-prev")) {
-        swiper.slidePrev();
-      }
-    });
+var imageno = 1;
+displayimg(imageno);
+
+function nextimg(n){
+  displayimg(imageno += n)
+}
+
+function currentSlide(n){
+  displayimg(imageno = n)
+}
+
+function displayimg(n){
+  var i;
+  var image = document.getElementsByClassName("Antecedentes_Slayer_Imagenes");
+  var dots = document.getElementsByClassName("dot");
+
+
+  if(n > image.length){
+    imageno = 1;
   }
-  
-  function resizeTextToFit() {
-    const quoteEls = document.getElementsByClassName('quote');
-    for (const el of quoteEls) {
-      el.style.width = el.offsetWidth;
-      el.style.height = el.offsetHeight;
-    }
-    textFit(quoteEls, { maxFontSize: 14 });
+  if(n < 1){
+    imageno = image.length;
   }
-  resizeTextToFit();
-  addEventListener("resize", (event) => {
-    resizeTextToFit();
-  });
+
+  for(i=0; i < image.length; i++){
+    image[i].style.display = "none "; 
+  }
+  for(i=0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active","");
+  }
+
+  image[imageno - 1].style.display="block";
+  dots[imageno - 1].className += "active";
+}
--- investigacion.php
+++ investigacion.php
@@ -19,6 +19,114 @@
             </div>
         </div>
     </header>
+<!--===============================================================================================
+                                            Parte 1
+==================================================================================================-->
+    <section class="Antecedentes">
+        <nav class="Antecedentes_Contenedor">
+            <div class="Antecedentes_Titulo">
+                <h1>Nuestros Antecedentes</h1>
+                <p>En 23 años de operación académica el área de Vinculación ha consolidado sus funciones que permiten dar respuesta a las necesidades actuales: se han incorporado a más de 2,905 estudiantes que han realizado sus residencias profesionales en los convenios que se tienen y han tenido con las empresas. </p>
+            </div>
+            <div class="Antecedentes_Slayer">
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Hospital Fernando Quiroz Gutiérrez</h1>
+                        <p>“Implementación del Departamento de Ingeniería Biomédica para el Hospital Fernando Quiroz Gutiérrez”.</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Smart Force</h1>
+                        <p>“Asistente virtual para la automatización de trámites, servicios y consulta de información”.</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Hospital General Chalco</h1>
+                        <p>“Sistema de gestión digital para el departamento de Ingeniería Biomédica del Hospital General Chalco”.</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Kristal Software</h1>
+                        <p>"Desarrollo de sistemas de información para administración, toma de decisiones, empresariales, gerenciales".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Hospital ISSSTE Tláhuac Heriberto Castillo 216</h1>
+                        <p>“El ingeniero biomédico como responsable sanitario y marco regulatorio mexicano”</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Smart Force</h1>
+                        <p>"Automatización para la captura de datos masivos".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Aviario el Nido</h1>
+                        <p>"Desarrollo e implementación de sistemas de información para control de personal, entradas y mantenimiento del aviario".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Sistemas de Seguridad y Tecnología de Oriente</h1>
+                        <p>"IOT para iluminación de salas de estar".</p>
+                    </div>
+                </div><div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Hospital Regional de Alta Especialidad de Ixtapaluca</h1>
+                        <p>"Desarrollo de un simulador de presión cerebral”.</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>CECYTEM Ixtapaluca</h1>
+                        <p>"Implementación de plataforma E-Learning".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Plaza Coatepec</h1>
+                        <p>"Rehabilitación de Plaza Coatepec con la Comisión de Fiestas Patronales Coatepec".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Proyecto de Vinculación Atrio del Templo </h1>
+                        <p>"Proyecto de Vinculación para el Mantenimiento Atrio del Templo de la Natividad de María en Coatepec con la Comisión de Fiestas Patronales Coatepec".</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Acceso al TESI </h1>
+                        <p>"Proyecto de pórtico de acceso al TESI con la empresa CONINGAR CONSTRUCCIONES S.A. DE C.V."</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Aulas provisionales en el Tecnológico de Estudios Superiores de Ixtapaluca</h1>
+                        <p>"11 Aulas provisionales en el Tecnológico de Estudios Superiores de Ixtapaluca realizado por la empresa GRUPO ORGANIZADOR Y CONSTRUCTOR 2F S.A. DE C.V."</p>
+                    </div>
+                </div>
+                <div class="Antecedentes_Slayer_Imagenes">
+                    <div class="img">
+                        <h1>Curso-Taller de Drones a cargo de la empresa CODEZUR.</h1>
+                        <p></p>
+                    </div>
+                </div>
+                
+                
+                <div class="botones">
+                    <a onclick="nextimg(-1)" class="prev">&#10094;</a>
+                    <a onclick="nextimg(1)" class="next">&#10095;</a>
+                </div>
+            </div>
+        </nav>
+    </section>
 
 <!--===============================================================================================
                                             Parte final
@@ -26,5 +134,8 @@
     <footer class="fin">
         <?php require('Global/footer.php');?>
     </footer> 
+
+
+<script src="JS/SlayerCambiante.js"></script>
 </body>
 </html>
