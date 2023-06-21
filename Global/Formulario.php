4ccbdcf640e5592d78c69f07685c720efa0edc2c
LuisLuna24 <eduarlun4@gmail.com>
2023-06-21 14:17:20 -0600

    el boton del formulario no envia y marca en rojo

--- Global/Formulario.php
+++ Global/Formulario.php
@@ -24,12 +24,13 @@
                     <option>Externo</option>
                 </optgroup>
             </select>
-            <input class="controls" type="text" name="Numero de telefono" id="Numero de telefono" placeholder="Numero de telefono" onkeydown="validateInt(event)">
-            <textarea class="controls" type="text" name="Mensaje" id="Mensaje" placeholder="Mensaje" onkeydown="validateInput(event)"></textarea>
+            <input class="controls" type="text" name="Numero de telefono" id="Numero de telefono" placeholder="Numero de telefono" onkeydown="validateInt(event)" oninput="limitInputLength(this, 10)">
+            <textarea class="controls_Area" type="text" name="Mensaje" id="Mensaje" placeholder="Mensaje" onkeydown="validateInput(event)"></textarea>
             <nav>
-                <input type="checkbox" name="" id="">Estoy deacuerdo con <a href="privacidad.php">Terminos y Condiciones</a></p></input>
+                <input type="checkbox" name="" id="myCheckbox">Estoy deacuerdo con <a href="privacidad.php">Terminos y Condiciones</a></input>
+                <h1 class="invalido" id="invalido">Primero hacepte terminos y condiciones</h1>
             </nav>
-            <button class="Btn_Registrar">Registrar
+            <button class="Btn_Registrar" id="myButton" onclick="validarCheckbox()">Registrar
                 <div class="star-1">
                     <svg xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><defs></defs><g id="Layer_x0020_1"><metadata id="CorelCorpID_0Corel-Layer"></metadata><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" class="fil0"></path></g></svg>
                 </div>
@@ -49,6 +50,7 @@
                     <svg xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><defs></defs><g id="Layer_x0020_1"><metadata id="CorelCorpID_0Corel-Layer"></metadata><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" class="fil0"></path></g></svg>
                 </div>
             </button>
+            
         </nav>
     </Section>
         <script src="JS/Validacion.js"></script>
--- JS/Validacion.js
+++ JS/Validacion.js
@@ -29,6 +29,38 @@ function validateInput(event) {
       // Si la tecla presionada no es un número, se evita la entrada
       event.preventDefault();
     }
+    
   }
 
- 
+  function limitInputLength(input, maxLength) {
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength); // Recortar el valor al máximo permitido
+    }
+  }
+
+const checkbox = document.getElementById('myCheckbox');
+const button = document.getElementById('myButton');
+const boton=document.getElementsByClassName("Btn_Registrar");
+
+checkbox.addEventListener('change', function() {
+  button.disabled = !checkbox.checked;
+  if (!checkbox.checked) {
+    boton.style.border="5px solid red";
+    
+  }
+});
+
+function validarCheckbox() {
+  var checkbox = document.getElementById('myCheckbox');
+  var button = document.getElementById('myButton'); 
+  var letras = document.getElementById('invalido');
+  if (!checkbox.checked) {
+    button.style.border="2px solid red";
+    button.style.color="red";
+    letras.style.opacity="1";
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+}
+
