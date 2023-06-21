4c9bbaf79721652cc5ec221a2a235b71d86cf19e
LuisLuna24 <eduarlun4@gmail.com>
2023-06-21 14:30:39 -0600

    el boton del formulario no envia y marca en rojo

---
+++ Global/Form_proyectos.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="CSS/Form_proyectos.css">
+</head>
+<body>
+    <section class="Form_Registro">
+        <nav class="Form_Registro_Contenedor">
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </nav>
+    </section>
+</body>
