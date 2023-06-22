078e3f8a448acb547e333878da3e88d92899b3b4
LuisLuna24 <eduarlun4@gmail.com>
2023-06-22 17:56:11 -0600

    el boton del formulario no envia y marca en rojo

--- Global/Form_proyectos.php
+++ Global/Form_proyectos.php
@@ -1,5 +1,6 @@
 <head>
     <link rel="stylesheet" href="CSS/Fom_proyectos.css">
+    <script src="JS/Form_Proyectos.js"></script>
 </head>
 <body>
     <section method="post" class="Form_Registro">
@@ -11,29 +12,39 @@
                 </div>
                 <p>Contactate con nosotros</p>
                 <div class="Redes_Sociales">
-                    <a href=""><img class="facebook2"src="img/facebook.svg" alt=""></a>
-                    <a href=""><img class="youtube2"src="img/youtube.svg" alt=""></a>
-                    <a href=""><img class="tesi2"src="img/tesi.svg" alt=""></a>
+                    <div class="Redes_Sociales_facebook">
+                        <a href=""><img src="img/facebook.svg" alt=""></a>
+                        <label class="titulo_facebook">Facebook</label>
+                    </div>
+                    <div class="Redes_Sociales_youtube">
+                        <a href=""><img src="img/youtube.svg" alt=""></a>
+                        <label class="titulo_youtube">Youtube</label>
+                    </div>
+                    <div class="Redes_Sociales_tesi">
+                        <a href=""><img src="img/tesi.svg" alt=""></a>
+                        <label class="titulo_tesi">Tesi</label>
+                    </div>
                 </div>
             </div>
             <div class="Form_Registro_Preguntas">
                 <label class="Registros_Label">Nombre Completo:</label>
-                <input type="text" class="Registro_Texto" placeholder="Nombre">
+                <input type="text" class="Registro_Texto" placeholder="Nombre" onkeydown="validateInput(event)">
                 <label class="Registros_Label">Empresa:</label>
-                <input type="text" class="Registro_Texto" placeholder="Empresa o Cargo">
+                <input type="text" class="Registro_Texto" placeholder="Empresa o Cargo" onkeydown="validateInput(event)">
                 <label class="Registros_Label">Correo:</label>
-                <input type="email" class="Registro_Texto"placeholder="Correo electronico">
+                <input type="email" class="Registro_Texto"placeholder="Correo electronico" onkeydown="myFunction(event)">
                 <label class="Registros_Label">Numero de Telefono:</label>
-                <input type="text" class="Registro_Texto" placeholder="Numero de telefono">
+                <input type="text" class="Registro_Texto" placeholder="Numero de telefono" onkeydown="validateInt(event)" oninput="limitInputLength(this, 10)">
                 <label class="Registros_Label">Mensaje:</label>
-                <Textarea class=Registro_Area placeholder="Escribe mensaje"></Textarea>
+                <Textarea class=Registro_Area placeholder="Escribe mensaje" onkeydown="validateInput2(event)" oninput="limitInputLength(this, 300)" ></Textarea>
                 
 
                 <div class="Form_Botones">
                     <nav>
-                        <input class="Check" type="checkbox" name="" id="myCheckbox">Accepto terminos de <a href="privacidad.php">Privacidad</a></input>
+                        <input class="Check" type="checkbox" name="" id="myCheckbox"">Accepto terminos de <a href="privacidad.php">Privacidad</a></input>
                     </nav>
-                    <input type="button" value="Enviar" class="boton_enviar">
+                    <button class="boton_enviar" id="myButton" onclick="validarCheckbox()">Registrar</button>
+                    
                 </div>
             </div>
         </form>
---
+++ JS/Form_Proyectos.js
@@ -0,0 +1,84 @@
+function validateInput(event) {
+    const key = event.key;
+    const regex = /^[a-zA-Z .]+$/; // Expresión regular para solo letras
+    
+    if (!regex.test(key)) {
+      // Si la tecla presionada no es una letra, se evita la entrada
+      event.preventDefault();
+    }
+  }
+  function validateInput2(event) {
+    const key = event.key;
+    const regex = /^[a-zA-Z .0-9]+$/; // Expresión regular para solo letras
+    
+    if (!regex.test(key)) {
+      // Si la tecla presionada no es una letra, se evita la entrada
+      event.preventDefault();
+    }
+  }
+
+
+  function myFunction(event) {
+    const key = event.key;
+    const regex = /^[a-zA-Z .@0-9_]+$/; // Expresión regular para solo letras
+    
+    if (!regex.test(key)) {
+      // Si la tecla presionada no es una letra, se evita la entrada
+      event.preventDefault();
+    }
+  }
+
+  function validateInt(event) {
+    const key = event.key;
+  
+    // Verificar si la tecla presionada es un número (0-9) o teclas especiales como backspace, delete, flechas de dirección, etc.
+    const isNumber = /^\d$/.test(key) || ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'].includes(key);
+  
+    if (!isNumber) {
+      // Si la tecla presionada no es un número, se evita la entrada
+      event.preventDefault();
+    }
+    
+  }
+
+  function limitInputLength(input, maxLength) {
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength); // Recortar el valor al máximo permitido
+    }
+  }
+
+const checkbox = document.getElementById('myCheckbox');
+const button = document.getElementById('myButton');
+const boton=document.getElementsByClassName("boton_enviar");
+
+checkbox.addEventListener('change', function() {
+  button.disabled = !checkbox.checked;
+  if (!checkbox.checked) {
+    boton.style.border="5px solid red";
+    
+  }
+});
+
+function validarCheckbox() {
+  var checkbox = document.getElementById('myCheckbox');
+  var button = document.getElementById('myButton'); 
+  var letras = document.getElementById('invalido');
+  if (!checkbox.checked) {
+    button.style.border="2px solid red";
+    button.style.color="red";
+    letras.style.opacity="1";
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+}
+
+
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que la página se recargue
+    
+    // Realiza las acciones de envío del formulario o procesamiento de datos aquí
+    
+    // Opcional: Restablece los campos del formulario
+    this.reset();
+  });
