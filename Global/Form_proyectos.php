1a34ab08b91d6cfc1f5601b0accf88232937779f
LuisLuna24 <eduarlun4@gmail.com>
2023-06-26 16:33:08 -0600

    modifique muho

--- Global/Form_proyectos.php
+++ Global/Form_proyectos.php
@@ -13,15 +13,15 @@
                 <p>Contactate con nosotros</p>
                 <div class="Redes_Sociales">
                     <div class="Redes_Sociales_facebook">
-                        <a href=""><img src="img/facebook.svg" alt=""></a>
+                        <a href="https://www.facebook.com/TESIOficial/?locale=es_LA"><img src="img/facebook.svg" alt=""></a>
                         <label class="titulo_facebook">Facebook</label>
                     </div>
                     <div class="Redes_Sociales_youtube">
-                        <a href=""><img src="img/youtube.svg" alt=""></a>
+                        <a href="https://www.youtube.com/@TESIOficial"><img src="img/youtube.svg" alt=""></a>
                         <label class="titulo_youtube">Youtube</label>
                     </div>
                     <div class="Redes_Sociales_tesi">
-                        <a href=""><img src="img/tesi.svg" alt=""></a>
+                        <a href="https://tesi.org.mx/"><img src="img/tesi.svg" alt=""></a>
                         <label class="titulo_tesi">Tesi</label>
                     </div>
                 </div>
