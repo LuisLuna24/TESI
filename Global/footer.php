c612df4b839b824bafc33756a0ef86bbb5989aef
LuisLuna24 <eduarlun4@gmail.com>
2023-06-26 16:42:37 -0600

    modifique muho

--- Global/footer.php
+++ Global/footer.php
@@ -7,15 +7,15 @@
         <h1 class="social_media_Titulo">Nuestros Sitios</h1>
             <div class="Redes_Sociales">
                 <div class="Redes_Sociales_facebook">
-                    <a href="https://www.facebook.com/TESIOficial/?locale=es_LA"><img src="img/facebook2.svg" alt=""></a>
+                    <a href="https://www.facebook.com/TESIOficial/?locale=es_LA"><img src="img/facebook.png" alt=""></a>
                     <label class="titulo_facebook">Facebook</label>
                 </div>
                 <div class="Redes_Sociales_youtube">
-                    <a href="https://www.youtube.com/@TESIOficial"><img src="img/youtube2.svg" alt=""></a>
+                    <a href="https://www.youtube.com/@TESIOficial"><img src="img/youtube.png" alt=""></a>
                     <label class="titulo_youtube">Youtube</label>
                 </div>
                 <div class="Redes_Sociales_tesi">
-                    <a href="https://tesi.org.mx/"><img src="img/tesi2.svg" alt=""></a>
+                    <a href="https://tesi.org.mx/"><img src="img/tesi.png" alt=""></a>
                     <label class="titulo_tesi">Tesi</label>
                 </div>
             </div>
