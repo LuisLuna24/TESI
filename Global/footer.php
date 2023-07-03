88c5b522705938373bf2686e8f49abcd3e41dd4d
LuisLuna24 <eduarlun4@gmail.com>
2023-07-02 22:14:59 -0600

    Termine de arregalr el fooat

--- Global/footer.php
+++ Global/footer.php
@@ -3,20 +3,28 @@
 </head>
 
 <div class="fin">
+
+    <div class="Redes_Sociales_Titulo">
+        <h1 class="Titulo">TESI</h1>
+    </div>
+    
     <div class="Redes_Sociales">
+        
         <div class="Redes_Sociales_Facebook">
-            <a href="" class="Facebook"><img src="img/facebook.png" alt=""></a>
+            <a href="https://www.facebook.com/TESIOficial" class="Facebook"><img src="img/facebook.png" alt=""></a>
             <label class="Facebook_Titulo">Facebook</label>
         </div>
 
         <div class="Redes_Sociales_Youtube">
-            <a href="" class="Youtube"><img src="img/youtube.png" alt=""></a>
+            <a href="https://www.youtube.com/@TESIOficial" class="Youtube"><img src="img/youtube.png" alt=""></a>
             <label class="Youtube_Titulo">Youtube</label>
         </div>
 
         <div class="Redes_Sociales_Tesi">
-            <a href="" class="Tesi"><img src="img/tesi.png" alt=""></a>
+            <a href="https://tesi.org.mx/" class="Tesi"><img src="img/tesi.png" alt=""></a>
             <label class="Tesi_Titulo">Tesi</label>
         </div>
     </div>
+
+
 </div>
