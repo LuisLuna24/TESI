1060aa4e2af594a8829d8ec481d96af736d3e38e
Sistemas 12 <sistemas12@sistemasccai.local>
2023-08-22 12:18:13 -0600

    Recuperacion de Pagina

--- investigacion.php
+++ investigacion.php
@@ -12,12 +12,6 @@
 ==================================================================================================-->
     <header>
         <?php require('Global/cabesera.php'); ?> 
-        <div  class="tituloCCAI">
-            <div id="divTitulo" class="divTitulo">
-                <h1>Investigación</h1>
-                <label>Bienvenido</label>
-            </div>
-        </div>
     </header>
     <section class="Cabesera">
         <div class="Cabesera_Titulo">
