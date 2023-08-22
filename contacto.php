12c04fbb790d7d815b5e0401916ea9414f340f1b
LuisLuna24 <eduarlun4@gmail.com>
2023-08-21 18:15:54 -0600

    Hola

--- contacto.php
+++ contacto.php
@@ -19,23 +19,24 @@
 <body>
     <header>
         <?php require('Global/cabesera.php'); ?> 
-        <div  class="tituloCCAI">
-            <div id="divTitulo" class="divTitulo">
-                <h1>Contacto</h1>
-                <label>Bienvenido</label>
-            </div>
-        </div>
     </header>
+    <section class="Cabesera">
+        <div class="Cabesera_Titulo">
+            <h1>Tecnológico de Estudios Superiores de Ixtapaluca</h1>
+            <h3>Bienvenido</h3>
+            <h5>PROYECTO DE CENTRO DE COOPERACIÓN ACADÉMICA – INDUSTRIA(CCAI) del <br> Tecnologico de Estudios Superiores de Ixtapalucas</h5>
+        </div>
+    </section>
 
     <section class="Titulo_Contacto">
-        <h2 class="Titulo_Contacto_Titulo">Ubicacion:</h2>
+        <h2 class="Titulo_Contacto_Titulo">Ubicación:</h2>
         <div class="Contenedor_Contenido">
             <nav class="informacion">
-                <label for="">Visitanos </label>
+                <label for="">Visítanos </label>
                 <iframe class="ubicacion" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.9093826969006!2d-98.84937000000005!3d19.37307599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1e0124f50dceb%3A0x8a0bcd921052a741!2sTecnol%C3%B3gico%20de%20Estudios%20Superiores%20de%20Ixtapaluca!5e0!3m2!1ses-419!2smx!4v1687882962271!5m2!1ses-419!2smx" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </nav>
             <nav class="boton">
-                <label for="">comunicate con nosotros, te contestaremos a la brevedad:</label>
+                <label for="">Comunícate con nosotros, te contestaremos a la brevedad:</label>
                 <button class="Btn_Registrar0">Mensaje
                     <div class="star-1">
                     <svg xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><defs></defs><g id="Layer_x0020_1"><metadata id="CorelCorpID_0Corel-Layer"></metadata><path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" class="fil0"></path></g></svg>
@@ -71,12 +72,12 @@
 
         <section class="Redes">
             <div class="Redes_Informacion">
-                <label class="Redes_Informacion_Direccion">Direccion: </label>
+                <label class="Redes_Informacion_Direccion">Dirección: </label>
                 <label>Carretera Coatepec, Callejon San Juan 7, 56580 Ixtapaluca, Méx.</label>
             </div>
 
             <div class="Redes_Informacion">
-                <label class="Redes_Informacion_Telefono">Telefono: </label>
+                <label class="Redes_Informacion_Telefono">Teléfono: </label>
                 <label>0000000000</label>
             </div>
 
